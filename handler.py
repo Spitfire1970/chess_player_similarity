687309c81ed9970b873b09b97aefbbda6a10d52e
Spitfire1970 <nakul.goyal.22@ucl.ac.uk>
2025-05-25 02:18:40 +0100

    handler

---
+++ handler.py
@@ -0,0 +1,29 @@
+import torch
+from encoder.model import Encoder
+ 
+class EndpointHandler():
+    def __init__(self, path="6.pt"):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        checkpoint = torch.load(path, self.device, weights_only=True)
+        self.model = Encoder(self.device)
+        state_dict = checkpoint['model_state']
+        self.model.load_state_dict(state_dict)
+        self.model = self.model.to(self.device)
+        self.model.eval()
+ 
+    def __call__(self, data):
+        tensor = torch.from_numpy(data.tensor).float().to(self.device)
+        if len(data) == 1:
+            with torch.no_grad():
+                embed = self.model(tensor)
+                embed = embed / torch.norm(embed)
+            return {"reply": embed.cpu().numpy()}
+        else:
+            with torch.no_grad():
+                embeds = self.model(tensor)
+                embeds = embeds.view((1, data.num_games, -1)).to(self.device)
+                centroids_incl = torch.mean(embeds, dim=1, keepdim=True)
+                centroids_incl = centroids_incl.clone() / torch.norm(centroids_incl, dim=2, keepdim=True)
+            centroids_incl = centroids_incl.cpu().squeeze(1)
+            final_embeds = centroids_incl[0].numpy().tolist()
+            return {"reply": final_embeds}
